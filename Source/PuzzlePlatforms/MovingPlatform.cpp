20ee32260b0a7b6b7ab3fbae0c8dd77b51766bda
Thomas <thomasterebesi@googlemail.com>
2020-03-19 15:37:49 +0100

    Authority and replication.

--- Source/PuzzlePlatforms/MovingPlatform.cpp
+++ Source/PuzzlePlatforms/MovingPlatform.cpp
@@ -11,11 +11,23 @@ AMovingPlatform::AMovingPlatform()
 	speed = 10.0;
 }
 
+void AMovingPlatform::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if (HasAuthority())
+	{
+		// these should not be called by the client
+		SetReplicates(true);
+		SetReplicateMovement(true);
+	}
+}
+
 void AMovingPlatform::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
-	if (HasAuthority())
+	if (HasAuthority())	// '!HasAuthority()' refers to the client only
 	{
 		FVector location = GetActorLocation();
 		location += FVector(speed * DeltaTime, 0, 0);
